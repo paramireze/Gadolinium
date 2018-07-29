4d2d8464d8e4d9937ca079bdd483ff7203a8b667
paramireze <paramireze@gmail.com>
2018-07-29 07:41:46 -0500

    my first commit

---
+++ Gadolinium/GadoliniumViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GadoliniumViewController.swift
+//  Gadolinium
+//
+//  Created by Ramirez Paul E on 7/29/18.
+//  Copyright © 2018 Ramirez Paul E. All rights reserved.
+//
+
+import Foundation
