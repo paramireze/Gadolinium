66f41e1ca6a961142a80d128bb98e664321bda2c
paramireze <paramireze@gmail.com>
2018-08-05 07:06:55 -0500

    refactored all the views to be top level and not nested in stackview

--- Gadolinium/GadoliniumViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GadoliniumViewController.swift
-//  Gadolinium
-//
-//  Created by Ramirez Paul E on 7/29/18.
-//  Copyright © 2018 Ramirez Paul E. All rights reserved.
-//
-
-import Foundation
